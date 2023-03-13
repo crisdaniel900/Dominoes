0f2c1f885d86b7ab5b49800024bcf3176d87900a
crisdaniel900 <126824633+crisdaniel900@users.noreply.github.com>
2023-03-13 20:41:32 +0000

    git push $ git push

---
+++ Dominoes/Program.cs
@@ -0,0 +1,47 @@
+﻿internal class Program
+{
+    internal class Domino
+    {
+        private int arriba;
+        private int abajo;
+        private int valor;
+
+        public Domino(int arriba, int abajo)
+        {
+            this.arriba = arriba;
+            this.abajo = abajo;
+        }
+
+         public static Domino operator +(Domino a, Domino b)
+        {
+            int arriba = a.arriba + b.arriba;
+            int abajo = a.abajo + b.abajo;
+            Domino resultado = new Domino(arriba, abajo);
+            return resultado;
+        }
+
+        public int GetArriba()
+        {
+            return arriba;
+        }
+
+        public int GetAbajo()
+        {
+            return abajo;
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        Domino a = new Domino(2, 3);
+        Domino b = new Domino(1, 4);
+
+        Domino resultado = a + b;
+
+        int sumaCombinada = resultado.GetArriba() + resultado.GetAbajo();
+
+        Console.WriteLine("Suma combinada: {0}", sumaCombinada);
+
+        Console.ReadKey();
+        } 
+    }
